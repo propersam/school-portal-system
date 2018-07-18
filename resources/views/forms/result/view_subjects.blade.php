078e6f8f483f467c09a341743886543ea5c69911
NCJoes <jcnwobodo@suretrade.ng>
2018-07-18 17:40:00 +0100

    Fix bugs

--- resources/views/forms/result/view_subjects.blade.php
+++ resources/views/forms/result/view_subjects.blade.php
@@ -107,13 +107,13 @@
 											<i class="fa fa-gift"></i> View by Subject
 										</div>
 										<div class="tools">
-											<a href="javascript:;" class="collapse">
+											<a href="javascript:" class="collapse">
 											</a>
 											<a href="#portlet-config" data-toggle="modal" class="config">
 											</a>
-											<a href="javascript:;" class="reload">
+											<a href="javascript:" class="reload">
 											</a>
-											<a href="javascript:;" class="remove">
+											<a href="javascript:" class="remove">
 											</a>
 										</div>
 									</div>
@@ -173,6 +173,4 @@
 		</div>
 	</div>
 	<!-- END CONTENT -->
-
-
 @endsection
