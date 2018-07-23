f3cd2a4e22355a4f73118120b683c474ee142c00
NCJoes <jcnwobodo@suretrade.ng>
2018-07-23 16:48:43 +0100

    Bug fix

--- resources/views/change_default_password.blade.php
+++ resources/views/change_default_password.blade.php
@@ -1,48 +1,40 @@
 <!DOCTYPE html>
-<!-- 
-Template Name: Metronic - Responsive Admin Dashboard Template build with Twitter Bootstrap 3.3.5
-Version: 4.1.0
-Author: KeenThemes
-Website: http://www.keenthemes.com/
-Contact: support@keenthemes.com
-Follow: www.twitter.com/keenthemes
-Like: www.facebook.com/keenthemes
-Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
-License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
--->
-<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
-<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if IE 8]>
+<html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]>
+<html lang="en" class="ie9 no-js"> <![endif]-->
 <!--[if !IE]><!-->
 <html lang="en">
 <!--<![endif]-->
 <!-- BEGIN HEAD -->
 <head>
-<meta charset="utf-8"/>
+    <meta charset="utf-8"/>
     <title>Change Default Password | SureEdu School Portal</title>
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta content="width=device-width, initial-scale=1.0" name="viewport"/>
-<meta http-equiv="Content-type" content="text/html; charset=utf-8">
-<meta content="" name="description"/>
-<meta content="" name="author"/>
-<!-- BEGIN GLOBAL MANDATORY STYLES -->
-<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css"/>
-<link href="/assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
-<link href="/assets/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css"/>
-<link href="/assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-<link href="/assets/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css"/>
-<!-- END GLOBAL MANDATORY STYLES -->
-<!-- BEGIN PAGE LEVEL STYLES -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+    <meta content="" name="description"/>
+    <meta content="" name="author"/>
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet"
+          type="text/css"/>
+    <link href="/assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+    <link href="/assets/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css"/>
+    <link href="/assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <link href="/assets/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css"/>
+    <!-- END GLOBAL MANDATORY STYLES -->
+    <!-- BEGIN PAGE LEVEL STYLES -->
 
-<link href="/assets/admin/pages/css/login3.css" rel="stylesheet" type="text/css"/>
-<!-- END PAGE LEVEL SCRIPTS -->
-<!-- BEGIN THEME STYLES -->
-<link href="/assets/css/components-rounded.css" id="style_components" rel="stylesheet" type="text/css"/>
-<link href="/assets/css/plugins.css" rel="stylesheet" type="text/css"/>
-<link href="/assets/admin/layout/css/layout.css" rel="stylesheet" type="text/css"/>
-<link href="/assets/admin/layout/css/themes/default.css" rel="stylesheet" type="text/css" id="style_color"/>
-<link href="/assets/admin/layout/css/custom.css" rel="stylesheet" type="text/css"/>
-<!-- END THEME STYLES -->
-<link rel="shortcut icon" href="favicon.ico"/>
+    <link href="/assets/admin/pages/css/login3.css" rel="stylesheet" type="text/css"/>
+    <!-- END PAGE LEVEL SCRIPTS -->
+    <!-- BEGIN THEME STYLES -->
+    <link href="/assets/css/components-rounded.css" id="style_components" rel="stylesheet" type="text/css"/>
+    <link href="/assets/css/plugins.css" rel="stylesheet" type="text/css"/>
+    <link href="/assets/admin/layout/css/layout.css" rel="stylesheet" type="text/css"/>
+    <link href="/assets/admin/layout/css/themes/default.css" rel="stylesheet" type="text/css" id="style_color"/>
+    <link href="/assets/admin/layout/css/custom.css" rel="stylesheet" type="text/css"/>
+    <!-- END THEME STYLES -->
+    <link rel="shortcut icon" href="favicon.ico"/>
 </head>
 <!-- END HEAD -->
 <!-- BEGIN BODY -->
@@ -50,7 +42,7 @@ License: You must have a valid license purchased only from themeforest(the above
 <!-- BEGIN LOGO -->
 <div class="logo">
     <a href="/">
-    <img src="/assets/admin/layout4/img/logo-big.png" alt=""/>
+        <img src="{{asset('assets/images/logo.png')}}" alt=""/>
     </a>
 </div>
 <!-- END LOGO -->
@@ -62,44 +54,46 @@ License: You must have a valid license purchased only from themeforest(the above
 <div class="content">
     <!-- BEGIN LOGIN FORM -->
     {{ Form::open(array('action' => 'RegisterController@changePassword' )) }}
-        {{ csrf_field() }}
-        <h3 class="form-title">Create your password</h3>
-        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-            <!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
-            <label class="control-label visible-ie8 visible-ie9">Password</label>
-            <div class="input-icon">
-                <i class="fa fa-lock"></i>
-                <input class="form-control placeholder-no-fix" type="password" autocomplete="off" placeholder="Password" name="password" value="{{ old('password') }}" required autofocus/>
-            </div>
-            @if ($errors->has('password'))
-                <span class="help-block">
+    {{ csrf_field() }}
+    <h3 class="form-title">Create your password</h3>
+    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+        <!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
+        <label class="control-label visible-ie8 visible-ie9">Password</label>
+        <div class="input-icon">
+            <i class="fa fa-lock"></i>
+            <input class="form-control placeholder-no-fix" type="password" autocomplete="off" placeholder="Password"
+                   name="password" value="{{ old('password') }}" required autofocus/>
+        </div>
+        @if ($errors->has('password'))
+            <span class="help-block">
                     <strong>{{ $errors->first('password') }}</strong>
                 </span>
-            @endif
-        </div>
-        <div class="form-group">
-            <label class="control-label visible-ie8 visible-ie9">Password</label>
-            <div class="input-icon">
-                <i class="fa fa-lock"></i>
-                <input id="password" class="form-control placeholder-no-fix" type="password" autocomplete="off" placeholder="Password (again)" name="password2" required/>
-                @if ($errors->has('password2'))
-                    <span class="help-block">
+        @endif
+    </div>
+    <div class="form-group">
+        <label class="control-label visible-ie8 visible-ie9">Password</label>
+        <div class="input-icon">
+            <i class="fa fa-lock"></i>
+            <input id="password" class="form-control placeholder-no-fix" type="password" autocomplete="off"
+                   placeholder="Password (again)" name="password2" required/>
+            @if ($errors->has('password2'))
+                <span class="help-block">
                         <strong>{{ $errors->first('password2') }}</strong>
                     </span>
-                @endif
-            </div>
+            @endif
         </div>
-        <div class="form-actions">
-            <button type="submit" class="btn green-haze pull-right">
+    </div>
+    <div class="form-actions">
+        <button type="submit" class="btn green-haze pull-right">
             Change <i class="m-icon-swapright m-icon-white"></i>
-            </button>
-        </div>
-       
-        
+        </button>
+    </div>
+
+
     </form>
     <!-- END LOGIN FORM -->
-   
-    
+
+
 </div>
 <!-- END LOGIN -->
 <!-- BEGIN COPYRIGHT -->
@@ -111,7 +105,7 @@ License: You must have a valid license purchased only from themeforest(the above
 <!-- BEGIN CORE PLUGINS -->
 <!--[if lt IE 9]>
 <script src="../../assets/global/plugins/respond.min.js"></script>
-<script src="../../assets/global/plugins/excanvas.min.js"></script> 
+<script src="../../assets/global/plugins/excanvas.min.js"></script>
 <![endif]-->
 <script src="/assets/plugins/jquery.min.js" type="text/javascript"></script>
 <script src="/assets/plugins/jquery-migrate.min.js" type="text/javascript"></script>
@@ -131,12 +125,12 @@ License: You must have a valid license purchased only from themeforest(the above
 <script src="/assets/admin/pages/scripts/login.js" type="text/javascript"></script>
 <!-- END PAGE LEVEL SCRIPTS -->
 <script>
-jQuery(document).ready(function() {     
-  
- 
-  Login.init();
- 
-});
+    jQuery(document).ready(function () {
+
+
+        Login.init();
+
+    });
 </script>
 <!-- END JAVASCRIPTS -->
 </body>
